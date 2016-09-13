768eac939fe686f5acabe63965ca6ddfcbe3e0aa
Trevor <trevorsemail96@gmail.com>
2016-09-13 13:51:08 -0500

    switch to new host

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const crypto = require('crypto');
 var db = require('./db.js');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.VAR || 3000;
 var info = [];
 var infoNextID = 1;
 
